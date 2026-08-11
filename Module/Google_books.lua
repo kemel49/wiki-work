fff0d5ef017a24c8742922cebf85d4731a417504
Kml <167911858+kemel49@users.noreply.github.com>
2026-08-11 18:00:33 +0530

    Add Google Books module
    New Scribunto Lua Module for transforming Google books template to lua module.

---
+++ Module/Google_books.lua
@@ -0,0 +1,37 @@
+local p = {}
+
+function p.main(frame)
+    local args = frame:getParent().args
+    -- Google Book id and Title and Plain URL value
+    local id = args["id"] or args[1]
+    local title = args["title"]
+    local plainUrl = args["plainurl"]
+
+    -- Check if ID is populated or not
+    if id == "" or id == nil then
+        return "<span class=error>Please specify an ID for Google Book</span>"
+    end
+    -- Check if ID is 12 character long or not
+    if #id ~= 12 then
+        return "<span class=error>Please enter a valid Google Books ID</span>"
+    end
+
+    -- URL Prefix
+    local urlPrefix = "https://books.google.com/books?id="
+    -- If Needed only Plain URL
+    if plainUrl == "yes" then
+        return urlPrefix..id
+    end
+
+    -- URL and text wrapped together
+    local wrapperText
+    if title == "" or title == nil then
+        wrapperText = mw.title.getCurrentTitle().text..'] at [[Google Books]]'
+    else
+        wrapperText = title..'] at [[Google Books]]'
+    end
+    -- Final output
+    return '['..urlPrefix..id..' '..wrapperText
+
+end
+return p
